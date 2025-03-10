df739bc430be97e907615b0938391d2c6d3bdb5b
gnjrk24 <gnjrk24@gmail.com>
2025-03-10 19:40:45 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = "こんにちは"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}
         </p>
         <a
           className="App-link"
@@ -20,6 +21,34 @@ function App() {
       </header>
     </div>
   );
+}
+
+export default App;*/
+
+/*import React from 'react'
+
+const App = () => {
+    return (
+        <h1>Hello, World!</h1>
+    )
+}
+
+export default App;*/
+
+
+import React from 'react'
+import Form from './Form'
+import List from './List'
+
+
+const App = () => {
+  return (
+      <React.Fragment>
+          <h1>Todo App<h1/>
+          <Form />
+          <List />
+      </React.Fragment>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>
 );
 
@@ -15,3 +15,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+const App = () => {
+  return (
+      <h1>Hello, World!</h1>
+  )
+}
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
